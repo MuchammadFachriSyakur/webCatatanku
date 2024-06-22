9c708f18e9c835866235467ed10c83130e2a14bb
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-22 19:26:18 +0700

    Update proses_edit_folder.php

--- proses_edit_folder.php
+++ proses_edit_folder.php
@@ -65,14 +65,19 @@ if(isset($_POST['updateFolder'])){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Notes || Formulir edit folder</title>
+  <link rel="stylesheet" href="src/css/form_edit.css" type="text/css" media="all" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-  <form method="POST">
-    <h1>Formulir edit folder</h1>
-    <input type="text" name="idFolder" value="<?= $idFolder; ?>" readonly>
-    <input type="text" name="username" value="<?= $usernameFolderPadaDatabase; ?>" readonly>
+  <form class="formEdit" method="POST">
+    <h1 class="title">Formulir edit folder</h1>
+    <input type="text" class="hidden" name="idFolder" value="<?= $idFolder; ?>" readonly>
+    <input type="text" class="hidden" name="username" value="<?= $usernameFolderPadaDatabase; ?>" readonly>
     <input type="text" name="namaFolder" value="<?= $namaFolderPadaDatabase; ?>">
-    <button type="submit" name="updateFolder" formaction="">Edit</button>
+    <a href="dashboard_user.php">Kembali</a>
+    <button type="submit" class="updateFolder" name="updateFolder" formaction="">Edit</button>
   </form>
 </body>
 </html>
