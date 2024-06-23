41ea3beb57d40e1330eb5715a9b02507825668c9
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-24 01:53:09 +0700

    Mengganti alamat file css

--- dashboard_user.php
+++ dashboard_user.php
@@ -31,7 +31,7 @@ if(isset($_GET['createFolder'])){
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Welcome to mynotes</title>
   <script src="https://unpkg.com/@phosphor-icons/web"></script>
-  <link rel="stylesheet" href="src/css/defaultHomePage.css" type="text/css" media="all" />
+  <link rel="stylesheet" href="src/css/detail_folder.css" type="text/css" media="all" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
