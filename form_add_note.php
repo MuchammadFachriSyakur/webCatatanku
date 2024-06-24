41e1d2c20a761c61dc21ed5fee24a4ee8bba61c1
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-24 18:37:37 +0700

    Mengupdate form_add_note.php
    Menambahkan url file css

--- form_add_note.php
+++ form_add_note.php
@@ -58,21 +58,25 @@ if($idFolder == 0 & $nameFolder == "" & $usernameToAddFolder == ""){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Notes || Create Notes</title>
+  <link rel="stylesheet" href="src/css/form_add_notes.css" type="text/css" media="all" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-  <form method="POST">
-    <input type="text" name="idFolder" value="<?= $idFolder; ?>" readonly required>
-    <input type="text" name="nameFolder" value="<?= $nameFolder; ?>" readonly required>
-    <input type="text" name="username" value="<?= $usernameToAddFolder; ?>" readonly required>
+  <form class="form_add_notes" method="POST">
+    <h1 class="title">Formulir Catatan</h1>
+    <input type="text" class="hidden" name="idFolder" value="<?= $idFolder; ?>" readonly required>
+    <input type="text" class="hidden" name="nameFolder" value="<?= $nameFolder; ?>" readonly required>
+    <input type="text" class="hidden" name="username" value="<?= $usernameToAddFolder; ?>" readonly required>
     <input type="text" name="titleNotes" placeholder="Masukan judul catatan anda" required>
     <input type="text" name="descriptionNotes" placeholder="Masukan deskripsi catatan anda" required>
     <select name="publish" required>
+      <option value="">Silahkan pilih publish atau private</option>
       <option value="Private">Private</option>
       <option value="Publis">Publis</option>
     </select>
+    <a href="detail_folder.php">Kembali</a>
     <button type="submit" name="tambahCatatan">Tambah</button>
   </form>
 </body>
