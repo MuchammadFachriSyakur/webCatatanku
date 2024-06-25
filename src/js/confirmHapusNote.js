fce4641e492d662f4d0ee5431516d0bdc6574192
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-26 06:55:11 +0700

    Add files via upload

---
+++ src/js/confirmHapusNote.js
@@ -0,0 +1,8 @@
+const btn_delete = document.querySelector("form button");
+
+btn_delete.addEventListener("click", (e)=>{
+   const konfirmasi = confirm("Apakah anda ingin menghapus folder");
+   if(konfirmasi == false){
+    e.preventDefault();
+   }
+});
