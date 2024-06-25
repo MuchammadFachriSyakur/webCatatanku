b53dd89a43411e55123038d9420807f7229dc8c9
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-25 15:54:21 +0700

    Update detail_note.php

--- detail_note.php
+++ detail_note.php
@@ -12,6 +12,7 @@ if(isset($_POST['detailNotes'])){
   $usernameFolder = htmlspecialchars($_POST['usernameFolder']);
   $NameFolder = htmlspecialchars($_POST['NameFolder']);
   $created_at = htmlspecialchars($_POST['created_at']);
+  $image = htmlspecialchars($_POST['image']);
 }else{
     echo "<script>
     window.location.href = 'dashboard_user.php';
@@ -34,14 +35,15 @@ if(!isset($_SESSION['username'])){
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <div class="background-notes" data-background='img/<?= $image; ?>'>
+     <p class="titleNotes"><?= $titleNotes; ?></p>
+    </div>
     <form action="" method="post">
       <input type="text" class="hidden" name="id" value="<?= $id; ?>" readonly>
 
       <input type="text" class="hidden" name="titleNotes" value="<?= $titleNotes; ?>" readonly>
 
       <input type="text" class="hidden" name="descriptionNotes" value="<?= $descriptionNotes; ?>" readonly>
-
-      <p class="titleNotes"><?= $titleNotes; ?></p>
 
       <p class="descriptionNotes"><?= $descriptionNotes; ?></p>
 
@@ -63,5 +65,7 @@ if(!isset($_SESSION['username'])){
         <button type="submit">Hapus</button>
       <?php endif; ?>
     </form>
+
+<script src="src/js/imageNote.js"></script>
 </body>
 </html>
