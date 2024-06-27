b18386cce214a24e3157caff99ae95c5c59cfba2
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-27 15:01:53 +0700

    Mengupdate dashboard_user.php
    1.Menambahkan pesan jika tidak ada data

--- dashboard_user.php
+++ dashboard_user.php
@@ -70,8 +70,8 @@ if(isset($_SESSION['username']) && isset($_SESSION['is_login_user'])){
         <li>
           <form method="POST">
             <input type="text" class="hidden" name="idFolder" value="<?= $data['id']; ?>" readonly>
-            <input type="text" class="hidden" name="usernameFolder" value="<?= $data['username']; ?>" readonly>
-            <input type="text" class="hidden" name="nameFolder" value="<?= $data['name']; ?>" readonly>
+            <input type="text" class="hidden" name="usernameFolder" value="<?= htmlspecialchars($data['username']); ?>" readonly>
+            <input type="text" class="hidden" name="nameFolder" value="<?= htmlspecialchars($data['name']); ?>" readonly>
             <button type="submit" name="detailFolder" class="nameFolder" formaction="detail_folder.php" style="color: rgb(204, 204, 204);"><?= $data['name']; ?></button>
             <div class="wrapAction">
               <button type="submit" class="editFolder" name="editFolder" formaction="proses_edit_folder.php"><i class="ph ph-pencil"></i></button>
@@ -92,58 +92,89 @@ if(isset($_SESSION['username']) && isset($_SESSION['is_login_user'])){
     
     <section class="informationDisplay">
       <div class="wrapHeader">
+
         <div class="wrap1">
           <h1>Mynotes</h1>
-          <img class="profilePicture" src="img/<?= $profilePicture; ?>" data-image="img/<?= $profilePicture; ?>"  alt="Profile Picture">
-          <img src="img/asset/menu.png" alt="toggle" class="toggle">
+          <img class="profilePicture" src="img/<?= $profilePicture; ?>" data-image="img/<?= $profilePicture; ?>" alt="Profile Picture" loading='lazy'>
+          <img src="img/asset/menu.png" alt="toggle" class="toggle" loading='lazy'>
         </div>
+
         <form action="" method="GET" class="searchData">
           <input type="search" name="searching" placeholder="Search..."/>
           <button type="submit">
             <img src="img/asset/search-interface-symbol.png" alt="Searcing Logo"/>
           </button>
         </form>
+
       </div>
       <div class="wrapNotesPublish">
         <?php 
+        $cekData = false;
         $sqlNotes = "SELECT * FROM notes";
         $queryNotes = mysqli_query($db,$sqlNotes);
         while($data = mysqli_fetch_array($queryNotes)):
         $publish = $data['publish'];
+        $id = $data["id"];
+        $titleNotes = $data['titleNotes'];
+        $descriptionNotes = $data['descriptionNotes'];
+        $publish = $data['publish'];
+        $idFolder = $data['idFolder'];
+        $usernameFolder = $data['usernameFolder'];
+        $NameFolder = $data['NameFolder'];
+        $created_at = $data['created_at'];
+        $image = $data['image'];
+        $view = $data['view'];
         ?>
-          <?php if($publish == "Publis"): ?>
-           <form action="detail_note_public.php" method="POST" class="notes">
+          <?php
+          if($publish == "Publis"){
+            $cekData = true;
+           echo "
+           <form action='detail_note_public.php' method='POST' class='notes'>
 
-            <input type="text" class="hidden" name="id" value="<?= $data['id']; ?>">
+            <input type='text' class='hidden' name='id' value='$id'>
 
-            <input type="text" class="hidden" name="titleNotes" value="<?= $data['titleNotes']; ?>" readonly>
+            <input type='text' class='hidden' name='titleNotes' value='$titleNotes' readonly>
 
-            <input type="text" class="hidden" name="descriptionNotes" value="<?= $data['descriptionNotes']; ?>" readonly>
+            <input type='text' class='hidden' name='descriptionNotes' value='$descriptionNotes' readonly>
 
-            <input type="text" class="hidden" name="publish" value="<?= $data['publish']; ?>" readonly>
+            <input type='text' class='hidden' name='publish' value='$publish' readonly>
 
-            <input type="text" class="hidden" name="idFolder" value="<?= $data['idFolder']; ?>">
+            <input type='text' class='hidden' name='idFolder' value='$idFolder'>
 
-            <input type="text" name="usernameFolder" class="hidden" value="<?= $data['usernameFolder']; ?>" readonly>
+            <input type='text' name='usernameFolder' class='hidden' value='$usernameFolder' readonly>
 
-            <input type="text" class="hidden" name="NameFolder" value="<?= $data['NameFolder']; ?>">
+            <input type='text' class='hidden' name='NameFolder' value='$NameFolder'>
 
-            <input type="text" class="hidden" name="created_at" value="<?= $data['created_at']; ?>">
+            <input type='text' class='hidden' name='created_at' value='$created_at'>
 
-            <input type="text" class="hidden" name="image" value="<?= $data['image']; ?>">
+            <input type='text' class='hidden' name='image' value='$image'>
 
-            <input type="text" class="hidden" name="view" value="<?= $data['view']; ?>">
+            <input type='text' class='hidden' name='view' value='$view'>
 
-            <button type="submit" name="detailNotes">
-              <p class="username"><?= $data['usernameFolder']; ?></p>
-              <p class="title"><?= $data['titleNotes']; ?></p>
-              <p class="description"><?= $data['descriptionNotes']; ?></p>
-              <p class="date"><?= $data['created_at']; ?></p>
+            <button type='submit' name='detailNotes'>
+              <p class='username'>$usernameFolder</p>
+              <p class='title'>$titleNotes</p>
+              <p class='description'>$descriptionNotes</p>
+              <p class='date'>$created_at</p>
             </button>
            </form>
-          <?php endif; ?>
+           ";
+          }       
+          ?>
         <?php endwhile; ?>
+
+        <?php if($cekData == false): ?>
+          <div class='noNote'>
+            <div class='card-information'>
+              <img src='img/asset/no-note-2.avif' alt='No Note' loading='lazy'>
+              <p class='title'>Tidak ada catatan yang dibagikan secara publik saat ini</p>
+              <p class='description'>Periksa kembali nanti untuk pembaruan!</p>
+            </div>
+          </div>
+        <?php endif; ?>
+
       </div>
+
     </section>
     
   </main>
