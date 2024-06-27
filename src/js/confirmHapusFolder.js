4208adfc0fdb66e8519b91c083c5743546b93a59
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-27 12:19:53 +0700

    Mengupdate confirmHapusFolder.js
    1.Memperbaiki selection elemen pada variable button dimana seharusnya saya mengetikan querySellectorAll akan tetapi saya mengetikan querySellector

--- src/js/confirmHapusFolder.js
+++ src/js/confirmHapusFolder.js
@@ -1,8 +1,10 @@
-const button = document.querySelector("main .nav-bar .wrapFolder li form .wrapAction .hapusFolder");
+const button = document.querySelectorAll("main .nav-bar .wrapFolder li form .wrapAction .hapusFolder");
 
-button.addEventListener("click", (e)=>{
+button.forEach((e)=>{
+ e.addEventListener("click" , (e)=>{
   const konfirmasi = confirm("Apakah anda ingin menghapus folder");
   if(konfirmasi == false){
     e.preventDefault();
   }
+ })
 });
