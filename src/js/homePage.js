a5938c0930cdd21eddbd12ed984b5034a4dfb4f3
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-27 13:04:03 +0700

    Mengupdate homePage.js
    1.Menambahkan fungsi supaya ketika diklik profile user maka gambar akan diperbesar dan akan tampak jelas

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,6 +1,7 @@
 const buttonShowAddFolder = document.querySelector("main .nav-bar .wrapCreateFolder .createdFolder");
 const buttonHiddenAddFolder = document.querySelector(".wrapAlertFormAdd .formCreateFolder .deleteAlertFolder .delete");
 const alertForm = document.querySelector(".wrapAlertFormAdd");
+const profilePicture = document.querySelector("main .informationDisplay .wrapHeader .wrap1 .profilePicture");
 
 buttonShowAddFolder.addEventListener("click", (e) =>{
   alertForm.classList.toggle("show");
@@ -9,3 +10,23 @@ buttonShowAddFolder.addEventListener("click", (e) =>{
 buttonHiddenAddFolder.addEventListener("click", e =>{
   alertForm.classList.toggle("show");
 });
+
+profilePicture.addEventListener("click", (e)=>{
+ const nameImage = e.target.getAttribute("data-image");
+ const popup = document.createElement('div');
+ popup.className = 'modalProfileImage';
+ popup.innerHTML = `
+  <section class="modalProfileImage">
+    <span class="closeModal" onclick="closeModal()"><i class="ph ph-x"></i></span>
+    <div class="wrapImage">
+     <img src="${nameImage}" alt="Profile Picture">
+    </div>
+  </section>
+ `;
+ document.body.appendChild(popup);
+});
+
+function closeModal(){
+  const popup = document.querySelector(".modalProfileImage");
+  document.body.removeChild(popup);
+}
