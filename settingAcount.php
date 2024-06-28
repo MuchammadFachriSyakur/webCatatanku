03196bcfc4f8776768385771976027aec2b5fb6d
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-06-28 09:57:40 +0700

    Update settingAcount.php

--- settingAcount.php
+++ settingAcount.php
@@ -6,8 +6,12 @@ $idUsername = "";
 $username = "";
 $usernameOne = "";
 $password = "";
+$passwordOne = "";
 $email = "";
 $image = "";
+
+//Contoh penggunaan ternary operator;
+$result = 15 > 14 ? "15 tidak lebih besar dari 14" : "15 tidak lebih besar dari 14";
 
 if(!isset($_SESSION['username']) && !isset($_SESSION['is_login_user']) ){
     echo "<script>
@@ -26,6 +30,7 @@ if(isset($_POST['settingAcount'])){
     $idUsername = $data['id'];
     $username = $data['username'];
     $usernameOne = $data['username'];
+    $passwordOne = $data['password'];
     $password = $data['password'];
     $email = $data['email'];
     $image = $data['image'];
@@ -40,26 +45,35 @@ if(isset($_POST['editAccount'])){
  $id = $_POST['idUsername'];
  $usernameEdit = htmlspecialchars($_POST['username']);
  $usernameOne = htmlspecialchars($_POST['usernameOne']);
- $pasword = htmlspecialchars($_POST['password']);
+ $password = htmlspecialchars($_POST['password']);
+ $passwordOne = htmlspecialchars($_POST['passwordOne']);
  $email = htmlspecialchars($_POST['email']);
+
+ $hashPassword = hash("sha256",$password);
  
  $image_name = $_FILES['image']['name'];
  $image_error = $_FILES['image']['error'];
  $image_size = $_FILES['image']['size'];
  $image_tmp_name = $_FILES['image']['tmp_name'];
+ $extentionImageValid = ['jpg','jpeg','png'];
 
  $noSpaceUsername = str_replace(' ','',$usernameEdit);
- $noSpacePassword = str_replace(' ','',$pasword);
+ $noSpacePassword = str_replace(' ','',$password);
  $validationNumberUsername = preg_match("/\d/",$usernameEdit);
  $validationEmailUsername = filter_var($email,FILTER_VALIDATE_EMAIL);
+
+ $sql = "SELECT * FROM user WHERE username='$usernameEdit'";
+ $query = mysqli_query($db,$sql);
+ $cekHasilQuery = mysqli_num_rows($query);
+
  
  if(strlen($usernameEdit) < 6){
     echo "Username kurang dari 6 digit";
  }elseif(strlen($usernameEdit) > strlen($noSpaceUsername)){
     echo "Pastikan usename yang anda masukan tidak mengandung spasi";
- }elseif(strlen($pasword) < 6){
+ }elseif(strlen($password) < 6){
     echo "Passowrd kurang dari 6 digit";
- }elseif(strlen($pasword) > strlen($noSpacePassword)){
+ }elseif(strlen($password) > strlen($noSpacePassword)){
     echo "Pastikan password yang anda masukan tidak mengandung spasi";
  }elseif($validationEmailUsername == false){
     echo "Kamu harus memasukan alamat email yang benar";
@@ -68,23 +82,48 @@ if(isset($_POST['editAccount'])){
  }elseif($usernameOne == $usernameEdit){
     echo "Anda tidak mengganti username!!!";
     //Tinggal melanjutkan sql atau mengganti yang lainnya
-    if($image_error == 4 ){
+    if($image_error === 4){
         echo "Tidak mengganti gambar";
         //Tinggal melanjutkan sql atau mengganti yang lainnya
-     }
+    }elseif($image_error === 0){
+      echo "Tidak ada kesalahan.File berhasil diunggah";
+    }
  }else{
-    $sql = "SELECT * FROM user WHERE username='$usernameEdit'";
-    $query = mysqli_query($db,$sql);
-    $cekHasilQuery = mysqli_num_rows($query);
-
     if($image_error == 4 ){
-        echo "Tidak mengganti gambar";
-        //Tinggal melanjutkan sql atau mengganti yang lainnya
         if($cekHasilQuery == 1){
-            echo "Username sudah ada";
+            echo "<script>alert('Nama pengguna tersebut sudah dipakai oleh pengguna lain.Silahkan pilih yang lain.'); window.location.href = 'index.php';</script>";
+        }else{
+          if($password == $passwordOne){
+            $sql = "UPDATE user SET username='$usernameEdit',email='$email' WHERE username='$usernameOne'";
+            $query = mysqli_query($db,$sql);
+
+            $resultQuery = $query ? "<script>alert('Berhasil pada password yang sama'); window.location.href = 'index.php';</>" : "<script>alert('Gagal pada password yang sama'); window.location.href = 'index.php';</script>";
+            echo $resultQuery;
+          }else{
+            $sql = "UPDATE user SET username='$usernameEdit',password='$hashPassword',email='$email' WHERE username='$usernameOne'";
+            $query = mysqli_query($db,$sql);
+
+            $resultQuery = $query ? "<script>alert('Berhasil pada password yang berbeda'); window.location.href = 'index.php';</script>" : "<script>alert('Gagal pada password yang berbeda'); window.location.href = 'index.php';</script>";
+            echo $resultQuery;
+          }
+
         }
-    }else{
-        
+    }elseif($image_error === 0){
+      $extentionImage = explode('.',$image_name);
+      $extentionImage = strtolower(end($extentionImage));
+
+      $newNameFile = uniqid();
+      $newNameFile .= '.';
+      $newNameFile .= $extentionImage;
+
+      if($cekHasilQuery == 1){
+         echo "Username sudah ada";
+         exit;
+      }elseif(!in_array($extentionImage,$extentionImageValid)){
+         echo "<script>alert('Jangan masukan gambar selain jpg,jpeg dan png!!!')</script>";
+      }elseif($image_size > 1000000){
+         echo "<script>alert('Pastikan gambar tidak melebihi 1mb!!!')</script>";
+      }
     }
  }
 }
@@ -99,11 +138,25 @@ if(isset($_POST['editAccount'])){
 </head>
 <body>
    <form class="form_add_notes" method="POST" enctype="multipart/form-data">
+
+    <p>Id username</p>
     <input type="text" name="idUsername" value="<?= $idUsername; ?>"><br><br>
+
+    <p>Username ubah</p>
     <input type="text" name="username" value="<?= $username; ?>"><br><br>
+
+    <p>Username awal</p>
     <input type="text" name="usernameOne" value="<?= $usernameOne; ?>"><br><br>
+
+    <p>Password ubah</p>
     <input type="text" name="password" value="<?= $password; ?>"><br><br>
+
+    <p>Password awal</p>
+    <input type="text" name="passwordOne" value="<?= $passwordOne; ?>"><br><br>
+
+    <p>Email</p>
     <input type="text" name="email" value="<?= $email; ?>"><br><br>
+
     <p>Gambar Sebelumnya:</p>
     <input type="image" src="img/<?= $image; ?>" alt=""><br><br>
     <input type="file" name="image" value="<?= $image; ?>">
