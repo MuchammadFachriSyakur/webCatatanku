a4c3be2fec068a9a22437ee2678c3206dc2151e6
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-07-01 08:07:31 +0700

    Update verification_code.php

--- verification_code.php
+++ verification_code.php
@@ -85,18 +85,51 @@ if(isset($_POST['changePassword'])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>My Notes || Verification Code</title>
+  <link rel="stylesheet" href="src/css/forgot_password.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet" />
+  <link rel="apple-touch-icon" sizes="180x180" href="img/asset/favicon_io/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/asset/favicon_io/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/asset/favicon_io/favicon-16x16.png">
+  <link rel="manifest" href="img/asset/favicon_io/site.webmanifest">
 </head>
 <body>
-  <?php if($checkingVerificationCode == "yes"): ?>
-    <form action="" method="POST">
-      <input type="text" name="id" readonly>
-      <input type="text" name="username" value="<?= $username; ?>" placeholder="Masukan username yang sesuai" readonly>
-      <input type="text" name="kode" value="<?= $kode; ?>" placeholder="Masukan username yang sesuai" readonly>
-      <p><?= $messageErrorPassword; ?></p>
-      <input type="text" name="password" placeholder="Masukan password dengan benar" required>
-      <button type="submit" name="changePassword">Ganti Password</button>
-    </form>
-  <?php endif; ?>
+    <header>
+      <a href="index.php">Kembali</a>
+      <?= "Verifikasi password : " . $checkingVerificationCode; ?>
+    </header>
+    <main>
+      <h1 class="title">Formulir Change Password</h1>
+      <?php if($checkingVerificationCode == "yes"): ?>
+      <form method="POST" class="formCheckedCodeVerification">
+        <label for="id" class="hidden">
+          Id :
+          <input type="text" name="id" readonly>
+        </label>
+
+        <label for="username" class="hidden">
+          Username :
+          <input type="text" name="username" value="<?= $username; ?>" placeholder="Masukan username yang sesuai" readonly>
+        </label>
+
+        <label for="kode" class="hidden">
+          Kode :
+          <input type="text" name="kode" value="<?= $kode; ?>" placeholder="Masukan username yang sesuai" readonly>
+        </label>
+
+        <p><?= $messageErrorPassword; ?></p>
+
+        <label for="password">
+          Password :
+          <input type="text" name="password" placeholder="Masukan password dengan benar" required>
+        </label>
+
+        <button type="submit" name="changePassword">C Password</button>
+      </form>
+      <?php endif; ?>
+    </main>
 </body>
 </html>
