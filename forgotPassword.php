1d96b49e160520eb9de14e9db34d5f062b8ae1d3
MuchammadFachriSyakur <146730273+MuchammadFachriSyakur@users.noreply.github.com>
2024-07-01 07:49:53 +0700

    Update forgotPassword.php

--- forgotPassword.php
+++ forgotPassword.php
@@ -57,33 +57,96 @@ if(isset($_GET['sending'])){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Catatan || forgot password</title>
-</head>
-<body>
-  
-  <form method="POST">
-    <input type="text" name="kodeVerifikasiEmail" value="<?= $kodeVerifikasiEmail; ?>">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Catatan || forgot password</title>
+    <link rel="stylesheet" href="src/css/forgot_password.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <header>
+      <a href="index.php">Kembali</a>
+    </header>
+    <main>
+      <h1 class="title">Formulir Verification Code</h1>
+      <form method="POST" class="formSendCodeVerification">
+        <label for="kodeVerifikasiEmail" class="hidden">
+          Kode verifikasi :
+          <input
+            type="text"
+            name="kodeVerifikasiEmail"
+            value="<?= $kodeVerifikasiEmail; ?>"
+            readonly
+          />
+        </label>
 
-    <input type="text" name="username" value="<?= $username; ?>">
+        <label for="username" class="hidden">
+          Username :
+          <input
+            type="text"
+            name="username"
+            value="<?= $username; ?>"
+            readonly
+          />
+        </label>
 
-    <input type="text" name="emailUser" value="<?= $emailUser; ?>">
+        <label for="emailUser" class="hidden">
+          Email :
+          <input
+            type="text"
+            name="emailUser"
+            value="<?= $emailUser; ?>"
+            readonly
+          />
+        </label>
 
-    <button type="submit" name="sendCode" formaction="proses_send_code-verification.php">Kirim kode</button>
-  </form>
+        <button
+          type="submit"
+          name="sendCode"
+          formaction="proses_send_code-verification.php"
+        >
+          Kirim kode
+        </button>
+      </form>
 
-  <br><br><br>
-  
-  <?php if($cekSending == 'iya'): ?>
-  <form method="GET">
-    <input type="text" name="username" value="<?= $userSending; ?>">
-    <input type="number" name="kode" placeholder="Masukan kode verifikasi">
-    <button type="submit" name="verification_code" formaction="verification_code.php">verifikasi code</button>
-  </form>
-  <?php endif; ?>
-  
-</body>
+      <?php if($cekSending == 'iya'): ?>
+      <form method="GET" class="formCheckedCodeVerification">
+        <label for="username" class="hidden">
+          Username :
+          <input
+            type="text"
+            name="username"
+            value="<?= $userSending; ?>"
+            readonly
+          />
+        </label>
+
+        <label for="kode">
+          Kode Verifikasi :
+          <input
+            type="number"
+            name="kode"
+            placeholder="Masukan kode verifikasi"
+            required
+          />
+        </label>
+
+        <button
+          type="submit"
+          name="verification_code"
+          formaction="verification_code.php"
+        >
+          verifikasi code
+        </button>
+      </form>
+      <?php endif; ?>
+    </main>
+  </body>
 </html>
